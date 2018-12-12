ec0d93a35785152d91b7b185d01a52ca9472c662
92arek <a.pamulski@gmail.com>
2018-12-12 14:40:52 +0100

    initial commit

---
+++ BankTransferApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankTransferManager.Models;
+using BankTransferManager.Services;
+
+namespace BankTransferApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.ReadLine();
+
+
+            var userlist = new List<User>();
+            LoginService ls = new LoginService(userlist);
+
+            ls.LoginUser("asd", "asd");
+
+            var stringlist = new List<string>()
+            {
+                "x1",
+                "x2",
+                "x3",
+                "x1",
+                "x4",
+                "x5",
+                "x2",
+            };
+
+            var x1list = stringlist.Where(x => x == "x1");
+
+            var fistValue = stringlist.First();
+            var lastValue = stringlist.Last();
+            var single = stringlist.Single(x => x == "x4");
+
+            Console.Write(x1list);
+            Console.WriteLine(fistValue);
+            Console.WriteLine(lastValue);
+            Console.WriteLine(single);
+
+            Console.ReadKey();
+
+
+            var singleError = stringlist.Single(x => x == "x1");
+
+        }
+    }
+}
---
+++ BankTransferManager/Services/LoginService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankTransferManager.Models;
+
+namespace BankTransferManager.Services
+{
+    public class LoginService
+    {
+        private readonly List<User> users;
+
+        public LoginService(List<User> users)
+        {
+            this.users = users;
+        }
+
+        public User LoginUser(string name, string password)
+        {
+            return users.SingleOrDefault(u => u.Name == name && u.Password == password);
+        }
+
+    }
+}
+ 
